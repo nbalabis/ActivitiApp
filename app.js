c4f5868b8ff5d293f23b605747415a1a1c051cec
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-05 00:27:24 -0700

    Create basic express app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+const port = 3000
+app.listen(port, console.log(`Listening on port: ${port}`))
