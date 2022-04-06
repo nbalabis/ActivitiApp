4a534fc36904218773f54ced6103995a78bea8dc
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-06 16:06:28 -0700

    Modify seeds to inculde acitivity themes

--- models/activity.js
+++ models/activity.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema
 
 const ActivitySchema = new Schema({
     title: String,
+    theme: {
+        type: String,
+        enum: ['Animals', 'Art and Design', 'Culture, Society, and Science', 'Drink', 'Food', 'Entertainment', 'History and Literature', 'Nature and Outdoors', 'Sightseeing, Shopping, and Transportation', 'Sports', 'Wellness']
+    },
     image: String,
     price: Number,
     description: String,
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Activity = require('../models/activity')
 const cities = require('./cities')
-const { pre, activities } = require('./seedHelpers')
+const { categories } = require('./seedHelpers')
 const { names } = require('./names')
 const axios = require('axios')
 
@@ -18,9 +18,11 @@ const seedDB = async () => {
     await Activity.deleteMany({})
     for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
-        const randomActivity = sample(activities)
+        const category = sample(categories)
+        const randomActivity = sample(category.activities)
         const activity = new Activity({
-            title: `${sample(pre)}${randomActivity} with ${sample(names)}`,
+            title: `${randomActivity} with ${sample(names)}`,
+            theme: category.theme,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             image: `https://source.unsplash.com/random/?${randomActivity}`,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus sit amet dictum sit amet justo donec. Magna fringilla urna porttitor rhoncus dolor purus non enim. Parturient montes nascetur ridiculus mus. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Quis hendrerit dolor magna eget est. Adipiscing bibendum est ultricies integer quis auctor elit. Neque convallis a cras semper. Non enim praesent elementum facilisis leo vel fringilla. Leo vel orci porta non. Sed id semper risus in hendrerit gravida rutrum. Mattis rhoncus urna neque viverra justo nec. Mauris ultrices eros in cursus turpis massa. A iaculis at erat pellentesque adipiscing. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in.',
