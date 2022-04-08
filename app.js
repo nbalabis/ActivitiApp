c25fe2fb7fb581154abaa702de056d40dd4762e5
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-07 23:41:00 -0700

    Add error handling for async errors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const Activity = require('./models/activity')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 
 const app = express()
 
@@ -25,39 +27,49 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/activities', async (req, res) => {
+app.get('/activities', catchAsync(async (req, res) => {
     const activities = await Activity.find({})
     res.render('activities/index', { activities })
-})
+}))
 
 app.get('/activities/new', (req, res) => {
     res.render('activities/new')
 })
 
-app.post('/activities', async (req, res) => {
+app.post('/activities', catchAsync(async (req, res, next) => {
+    if(!req.body.activity) throw new ExpressError('Invalid Campground Data', 400)
     const activity = new Activity(req.body.activity)
     await activity.save()
     res.redirect(`/activities/${activity._id}`)
-})
+}))
 
-app.get('/activities/:id', async (req, res) => {
+app.get('/activities/:id', catchAsync(async (req, res) => {
     const activity = await Activity.findById(req.params.id)
     res.render('activities/show', { activity })
-})
+}))
 
-app.get('/activities/:id/edit', async (req, res) => {
+app.get('/activities/:id/edit', catchAsync(async (req, res) => {
     const activity = await Activity.findById(req.params.id)
     res.render('activities/edit', { activity })
-})
+}))
 
-app.put('/activities/:id', async (req, res) => {
+app.put('/activities/:id', catchAsync(async (req, res) => {
     const activity = await Activity.findByIdAndUpdate(req.params.id, { ...req.body.activity })
     res.redirect(`/activities/${activity._id}`)
-})
+}))
 
-app.delete('/activities/:id', async(req, res) => {
+app.delete('/activities/:id', catchAsync(async (req, res) => {
     const activity = await Activity.findByIdAndDelete(req.params.id)
     res.redirect('/activities')
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500, message = 'Something went wrong!'} = err
+    res.status(statusCode).send(message)
 })
 
 const port = 3000
