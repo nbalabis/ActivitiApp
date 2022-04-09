b79b189ff5858fa5a6746378a8fd967bc207d6a2
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-09 16:20:10 -0700

    Configure passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 const ExpressError = require('./utils/ExpressError')
 
 const activites = require('./routes/activities')
@@ -37,6 +40,12 @@ app.use(session({
     }
 }))
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
