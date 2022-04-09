0d20090689a42d44830547b8540da028638c1e6d
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-09 14:23:16 -0700

    Refactor js & css into public directory

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/activities', activites)
 app.use('/activities/:id/reviews', reviews)
