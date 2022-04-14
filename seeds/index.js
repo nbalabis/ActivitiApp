5cbf3b47132b2170b4e766c0ae306a99505ab576
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-13 23:32:41 -0700

    Update seed images

--- seeds/index.js
+++ seeds/index.js
@@ -61,7 +61,12 @@ const seedActivities = async () => {
             host: randomUser._id,
             theme: category.theme,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            image: `https://source.unsplash.com/random/?${randomActivity}`,
+            images: [
+                {
+                    url: `https://source.unsplash.com/random/?${randomActivity}`,
+                    filename: 'randomImg'
+                }
+            ],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus sit amet dictum sit amet justo donec. Magna fringilla urna porttitor rhoncus dolor purus non enim. Parturient montes nascetur ridiculus mus. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Quis hendrerit dolor magna eget est. Adipiscing bibendum est ultricies integer quis auctor elit. Neque convallis a cras semper. Non enim praesent elementum facilisis leo vel fringilla. Leo vel orci porta non. Sed id semper risus in hendrerit gravida rutrum. Mattis rhoncus urna neque viverra justo nec. Mauris ultrices eros in cursus turpis massa. A iaculis at erat pellentesque adipiscing. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in.',
             price: Math.floor(Math.random() * 90) + 10
         })
