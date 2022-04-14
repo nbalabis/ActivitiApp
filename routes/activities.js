a6d56933e24adf55b3bf04d12d858feab6a7609e
nbalabis <88114907+nbalabis@users.noreply.github.com>
2022-04-13 22:42:35 -0700

    Set up multer middleware to handle multipart/form-data

--- routes/activities.js
+++ routes/activities.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const multer  = require('multer')
 const catchAsync = require('../utils/catchAsync')
 const activities = require('../controllers/activities')
 const { validateActivity, validateId, isLoggedIn, isHost } = require('../middleware')
 const router = express.Router()
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get(catchAsync(activities.index))
     .post(isLoggedIn, validateActivity, catchAsync(activities.create))
+    // .post(upload.single('image'), (req, res) => {
+    //     res.send(req.body, req.file)
+    // })
 
 router.get('/new', isLoggedIn, activities.renderNewForm)
 
